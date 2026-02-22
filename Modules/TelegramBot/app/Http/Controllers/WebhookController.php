a536021415363f155efba2539a78c5bcd64b019b
miki-babi <mikiyasshiferaw@gmail.com>
2026-02-22 23:35:17 +0300

    test

--- Modules/TelegramBot/app/Http/Controllers/WebhookController.php
+++ Modules/TelegramBot/app/Http/Controllers/WebhookController.php
@@ -266,7 +266,7 @@ class WebhookController extends Controller
                             [
                                 [
                                     'text' => $launchPayload['button_text'],
-                                    'style' => "blue",
+                                    'style' => "primary",
                                     'web_app' => [
                                         'url' => $launchPayload['url'],
                                     ],
